26657e71b7053386fa31df9c7e8292fb8e6b9ead
Vallem Balu <vbnreddy5@gmail.com>
2021-08-25 11:35:02 +0530

    Valid Pin code with space

--- RegularExpressions/PINCode.js
+++ RegularExpressions/PINCode.js
@@ -19,3 +19,24 @@ let validateWithoutSpace = () =>
     }
 }
 validateWithoutSpace();
+let validateWithSpace = () =>
+{
+    console.log("\nvalidateWithSpace");
+    try
+    {
+        let pincode = prompt("Enter pincode:");// string 
+        //regex pattern for pincode not allowing spaces
+        let pincodePattern = new RegExp('^[1-9]{1}[0-9]{2}\\s?[0-9]{3}$');
+        //test pincode with pattern
+        if(pincodePattern.test(pincode))
+            console.log('pincode is valid');
+        else
+            throw 'Error!!pincode is not valid';
+    }
+    catch(e)
+    {
+        //catch the error and print it
+        console.error(e);
+    }
+}
+validateWithSpace();
